a5220defa3913b7ad7a08bc0a69d1494d26bf112
Illya Kyrychenko <kyrychenko.illya@gmail.com>
2024-02-12 23:49:08 +0200

    New updates

--- src/views/components/index.js
+++ src/views/components/index.js
@@ -9,7 +9,24 @@ var email = emailsend.value;
 var messagesend = document.getElementById("message");
 var message = messagesend.value;
 var button = document.getElementById("button");
+var burger = document.getElementsByClassName("head-burger")[0];
+var menu = document.getElementsByClassName("menu")[0];
 
+clickCounter = 0;
+
+burger.addEventListener('click', function(){
+    burger.classList.add("active")
+    menu.classList.add("active")
+    
+    clickCounter++;
+        if (clickCounter == 2) {
+            burger.classList.remove("active")
+    menu.classList.remove("active")
+    clickCounter = 0;
+    
+        }
+    
+})
 
 
 
